5eff53ab7f3637ed9be4f7033c34362f344155d0
Diogo Borges <diogomnborges@ua.pt>
2024-05-08 19:29:09 +0100

    update

--- paco1.5/src/pages/Disciplina.tsx
+++ paco1.5/src/pages/Disciplina.tsx
@@ -11,29 +11,29 @@ function Disciplina() {
     <div>
         <NavBar/>
         <div className='max-w-7xl h-fit flex flex-col m-auto mt-28'>
-            <h2 className='text-5xl w-full text-center font-bold' style={{color: '#333'}}>Compiladores</h2>
+            <h2 className='text-5xl w-full text-center font-bold' style={{color: '#333'}}>Compiladores - P</h2>
             <div className='grid grid-cols-2 gap-8 w-full mt-4 text-center'>
                 <div>
                     <span className='text-2xl font-bold' style={{color: '#333', fontSize: '28px'}}>Sumários</span>
                     <div className='flex flex-col gap-1 ' style={{fontSize: '22px'}}>
                         <div className='bg-dark text-white p-4 grid grid-cols-[40%_40%_10%]'>
-                            <span>Aula 6</span>
-                            <span>8/4/2024</span>
+                            <span>Aula 1</span>
+                            <span>7/4/2024</span>
                             <span><u>Sumário</u></span>
                         </div>
                         <div className='bg-dark text-white p-4 grid grid-cols-[40%_40%_10%]'>
-                            <span>Aula 6</span>
-                            <span>8/4/2024</span>
+                            <span>Aula 2</span>
+                            <span>14/4/2024</span>
                             <span><u>Sumário</u></span>
                         </div>
                         <div className='bg-dark text-white p-4 grid grid-cols-[40%_40%_10%]'>
-                            <span>Aula 6</span>
-                            <span>8/4/2024</span>
+                            <span>Aula 3</span>
+                            <span>21/4/2024</span>
                             <span><u>Sumário</u></span>
                         </div>
                         <div className='bg-dark text-white p-4 grid grid-cols-[40%_40%_10%]'>
-                            <span>Aula 6</span>
-                            <span>8/4/2024</span>
+                            <span>Aula 4</span>
+                            <span>28/4/2024</span>
                             <span><u>Sumário</u></span>
                         </div>
                     </div>
@@ -42,8 +42,8 @@ function Disciplina() {
                     <span  className='text-2xl font-bold' style={{color: '#333', fontSize: '28px'}}>Faltas(1)</span>
                     <div className='flex flex-col gap-1 '>
                         <div className='bg-dark text-white p-4 grid grid-cols-[1fr_1fr] items-center text-center justify-center' style={{fontSize: '22px'}}>
-                            <span>Aula 6</span>
-                            <span>8/4/2024</span>
+                            <span>Aula 3</span>
+                            <span>21/4/2024</span>
                         </div>
                     </div>
                 </div>
